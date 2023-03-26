87bb017e22de82846dd024d73269c6706d2055c8
Staedlmar <martin.staedler@web.de>
2023-03-26 21:37:18 +0200

    added test functionality to switch on/off relay 1/2

--- SolarEnergyManager.ino
+++ SolarEnergyManager.ino
@@ -10,9 +10,12 @@
 #include <RCSwitch.h>  // remote control (DEBO 433 RX) for wireless sockets
                        // library source folder: "rc-switch-2.6.4"
 
-#include <ArduinoRS485.h>  // Waveshare RS485 communication board
+//#include <ArduinoRS485.h>  // Waveshare RS485 communication board
                            // for communication with power measurement
                            // library source folder: "ArduinoRS485-1.0.5"
+
+#include <ModbusMaster.h>  // do we need the ArduinoRS485.h library or
+                           // is it suficient to only habe the Modbus lib?
 
 // ############################### defines #################################
 #define REMOTECTRL 10  // signal remote control transmit at input 10
@@ -31,6 +34,7 @@ bool bBBBB = false;  // BBBB
 // ######################### create classes ################################
 RCSwitch mySwitch = RCSwitch();  // remote control
 
+ModbusMaster RelayBoard; // instantiate ModbusMaster object
 
 // #########################################################################
 // ########################     i n i t     ################################
@@ -42,8 +46,15 @@ void setup() {
   mySwitch.enableTransmit(REMOTECTRL);
 
   // set baud rate for RS485 communication
-  RS485.begin(9600);
+  //RS485.begin(9600);
 
+  // communicate with Modbus Relay Board (default adress 255) over Serial (port 0)
+  RelayBoard.begin(255, Serial);
+  // Questions:
+  // - how to set baudrate
+  // - the relay module default adress is 255.
+  //   - how can this adress be changed?
+  // - which port of the Arduino is used? is this the TX?
 
   Serial.println("init finished");
 }
@@ -53,6 +64,15 @@ void setup() {
 // #########################################################################
 void loop() {
 
+   // test code switching on/off relay 1 and 2 in 1s cycle
+   RelayBoard.writeSingleCoil(0x0000, 0xFF); // switch ON (0xFF) relay 1 (0x0000);
+   delay(1000);
+   RelayBoard.writeSingleCoil(0x0001, 0xFF); // switch ON (0xFF) relay 2 (0x0001);
+   delay(1000);
+   RelayBoard.writeSingleCoil(0x0000, 0x00); // switch OFF (0x00) relay 1 (0x0000);
+   delay(1000);
+   RelayBoard.writeSingleCoil(0x0001, 0x00); // switch OFF (0x00) relay 2 (0x0001);
+   delay(1000) ;
 
   //Serial.println("loop finished");
 }
